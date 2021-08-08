84827fca677e7bae0c91935191e48d1e3975c116
Sean Behan <codebam@riseup.net>
2021-08-08 17:40:30 -0400

    add image gallery

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,7 +1,23 @@
 import "./Content.css";
+import ImageGallery from "react-image-gallery";
+
+const images = [
+  {
+    original: "https://picsum.photos/id/1018/1000/600/",
+    thumbnail: "https://picsum.photos/id/1018/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1015/1000/600/",
+    thumbnail: "https://picsum.photos/id/1015/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1019/1000/600/",
+    thumbnail: "https://picsum.photos/id/1019/250/150/",
+  },
+];
 
 const Content: React.FC = () => {
-  return <></>;
+  return <ImageGallery items={images} />;
 };
 
 export default Content;
